f9fa620bf1017f1ffc1cefcdc58cefa4a53bb633
Igor Ryabchuk <riabtchuk@gmail.com>
2018-06-20 16:37:54 +0100

    Add Jasmine 3.1.0 installation

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,9 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
+  it("should set the item name on creation", function() {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual("foo");
   });
 
 });
