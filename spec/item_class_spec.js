82016507df6ff01ad772105acbe96326fad8e599
Igor Ryabchuk <riabtchuk@gmail.com>
2018-06-21 18:11:02 +0100

    Create a working test for Item Class

--- spec/item_class_spec.js
+++ spec/item_class_spec.js
@@ -2,8 +2,7 @@ var Item = require('../src/item_class')
 
 describe('Item Class', function () {
     it('should create an item object', function () {
-      var item = new Item
-      gildedRose.items.push(new Item('Sword of Redemption', 10, 40))
-      expect(gildedRose.items[0].name).toEqual('Sword of Redemption')
+      var item = new Item('Sword of Redemption', 10, 40)
+      expect(item.name).toEqual('Sword of Redemption')
     });
 });
