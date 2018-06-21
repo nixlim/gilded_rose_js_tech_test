290851bd762ecfdd95caa8827c5dccd3ac46e098
Igor Ryabchuk <riabtchuk@gmail.com>
2018-06-21 11:44:38 +0100

    Add Jasmine and Istanbul as NPM packages
    Create and configure package.json.
    
    Remove standalone Jasmine.
    
    Add Jasmine for Node.js as an NPM module to facilitate CLI testing. Configure jasmine.json to run relevant tests.
    
    Add Istanbul for Node.js as an NPM module to facilitate code coverage testing.

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,25 @@
-describe("Gilded Rose", function() {
+var Shop = require('../src/gilded_rose')
 
-  it("should set the item name on creation", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("foo");
-  });
+describe ("Shop Class", function () {
+  var gildedRose = new Shop()
+  beforeEach(function () {
 
-});
+    gildedRose.items.push({ name: 'Aged Brie', sellIn: 2, quality: 0 })
+    gildedRose.items.push({ name: 'Elixir of the Mongoose', sellIn: 5, quality: 7 })
+    gildedRose.items.push({ name: 'Sulfuras, Hand of Ragnaros', sellIn: 0, quality: 80 })
+    gildedRose.items.push({ name: 'Sulfuras, Hand of Ragnaros', sellIn: -1, quality: 80 })
+    gildedRose.items.push({ name: 'Backstage passes to a TAFKAL80ETC concert', sellIn: 15, quality: 20 })
+    gildedRose.items.push({ name: 'Backstage passes to a TAFKAL80ETC concert', sellIn: 10, quality: 49 })
+    gildedRose.items.push({ name: 'Backstage passes to a TAFKAL80ETC concert', sellIn: 5, quality: 49 })
+    // this conjured item does not work properly yet
+    gildedRose.items.push({ name: 'Conjured Mana Cake', sellIn: 3, quality: 6 })
+  })
+  describe ('Normal Items', function () {
+    it("should decrease the #sellIn and #quality of a normal item by 1", function () {
+      gildedRose.items.push({ name: '+5 Dexterity Vest', sellIn: 10, quality: 20 })
+      let items = gildedRose.items
+      expect(items[0].sellIn).toEqual(9)
+    })
+  })
+
+})
